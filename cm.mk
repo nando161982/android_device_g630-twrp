95fc7076c5ef9374890eb4585294c35969cf67a1
nando161982 <nando161982@gmail.com>
2016-01-24 14:24:47 -0430

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,14 +2,14 @@
 PRODUCT_RELEASE_NAME := G630
 
 # Inherit from the common open source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration.
-$(call inherit-product, device/huawei/g630/device.mk)
+$(call inherit-product, device/huawei/kvy625-u/device_kvy625-u.mk)
 
 # Device identifier.
-PRODUCT_DEVICE := G630
-PRODUCT_NAME := cm_g630
+PRODUCT_DEVICE := kvy625-u
+PRODUCT_NAME := cm_kvy625-u
 PRODUCT_BRAND := huawei
-PRODUCT_MODEL := Ascend G630
+PRODUCT_MODEL := kvy625-u
 PRODUCT_MANUFACTURER := huawei
